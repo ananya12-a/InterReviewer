eee7473d887519c5ec038d6339e145e78f2a4cc7
Ananya Aggarwal <ananyaag@andrew.cmu.edu>
2024-02-02 18:42:28 -0500

    upload file

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <v-app>
     <v-main>
+      <p>
+        <router-link to="/">Go to Home</router-link>
+        <router-link to="/upload">Go to Upload</router-link>
+      </p>
       <router-view />
     </v-main>
   </v-app>
