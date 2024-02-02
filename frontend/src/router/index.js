f494ebfd6ce3b8e3c8870d4dab09fa741cdeca30
Ananya Aggarwal <ananyaag@andrew.cmu.edu>
2024-02-02 18:06:48 -0500

    vuetify

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -6,11 +6,11 @@
 
 // Composables
 import { createRouter, createWebHistory } from 'vue-router/auto'
-import { setupLayouts } from 'virtual:generated-layouts'
+// import { setupLayouts } from 'virtual:generated-layouts'
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  extendRoutes: setupLayouts,
+  // extendRoutes: setupLayouts,
 })
 
 export default router
