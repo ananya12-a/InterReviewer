ddb5ab79f887bf1b3e0eb8beced63947602b05d0
Ananya Aggarwal <ananyaag@andrew.cmu.edu>
2024-02-02 23:39:07 -0500

    idk

--- backend/analysis.py
+++ backend/analysis.py
@@ -6,6 +6,6 @@ import os
 with zipfile.ZipFile("job_output/artifacts.zip", 'r') as zip_ref:
     zip_ref.extractall("job_output")
 
-dir = "backend/job_output/file-0-video1124829211.mp4/csv/video1124829211.mp4"
+dir = "job_output/file-0-video1318490298.mp4/csv/video1318490298.mp4"
 os.chdir(dir)
 
