32c54647c033ea19ae5b64480b8c136f985f656d
Sneha Jaiswal <snehajai@andrew.cmu.edu>
2024-02-02 23:10:37 -0500

    artifacts extracting

--- backend/analysis.py
+++ backend/analysis.py
@@ -1,2 +1,11 @@
 import pandas as pd
 import numpy as np
+import zipfile
+import os
+
+with zipfile.ZipFile("job_output/artifacts.zip", 'r') as zip_ref:
+    zip_ref.extractall("job_output")
+
+dir = "backend/job_output/file-0-video1124829211.mp4/csv/video1124829211.mp4"
+os.chdir(dir)
+
