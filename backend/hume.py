ac66fe79f20d80556d629c49fb4d494f68628215
Sneha Jaiswal <snehajai@andrew.cmu.edu>
2024-02-02 22:19:31 -0500

    updating

--- backend/hume.py
+++ backend/hume.py
@@ -1,7 +1,20 @@
 from hume import HumeBatchClient
 from hume.models.config import FaceConfig
+from hume.models.config import ProsodyConfig
 
 client = HumeBatchClient("130VUokCECUiWliyfsZDdrNOpImkLWv60RrPSVPAl1RycIWk")
-urls = ["https://iep.utm.edu/wp-content/media/hume-bust.jpg"]
-config = FaceConfig()
-job = client.submit_job(urls, [config])
+# urls = ["https://storage.googleapis.com/hume-test-data/video/armisen-clip.mp4"]
+filepaths = ["/user_data/zoom_mtg.mp4"]
+configs = [FaceConfig(identify_faces=True), ProsodyConfig()]
+# job = client.submit_job(urls, configs)
+job = client.submit_job(None, [configs],file=filepaths)
+
+print(job)
+print("Running...")
+
+details = job.await_complete()
+job.download_predictions("predictions.json")
+print("Predictions downloaded to predictions.json")
+
+job.download_artifacts("artifacts.zip")
+print("Artifacts downloaded to artifacts.zip")
