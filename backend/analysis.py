48344918e4c5f9286fb7768cf21017f3703cbacc
Sneha Jaiswal <snehajai@andrew.cmu.edu>
2024-02-03 01:25:00 -0500

    analysis template almost done:

--- backend/analysis.py
+++ backend/analysis.py
@@ -6,37 +6,46 @@ import os
 # with zipfile.ZipFile("job_output/artifacts.zip", 'r') as zip_ref:
 #     zip_ref.extractall("job_output")
 
-dir = "job_output/file-0-video1318490298.mp4/csv/video1318490298.mp4"
-os.chdir(dir)
+dir = "job_output/file-0-video1318490298.mp4/csv/video1318490298.mp4/"
+#os.chdir(dir)
 
-face = pd.read_csv("face.csv")
-prosody = pd.read_csv("prosody.csv")
+face = pd.read_csv(dir+"face.csv")
+prosody = pd.read_csv(dir+"prosody.csv")
 
+viewer_face = "0"
+viewee_face = "1"
+viewer_pros = "0"
+viewee_pros = "1"
 
-## face dataset processing
+## dataset processing
 emotions = ['Admiration', 'Adoration', 'Aesthetic Appreciation', 'Amusement', 'Anger', 'Anxiety', 'Awe', 'Awkwardness', 'Boredom', 'Calmness', 'Concentration', 'Contemplation', 'Confusion', 'Contempt', 'Contentment', 'Craving', 'Determination', 'Disappointment', 'Disgust', 'Distress', 'Doubt', 'Ecstasy', 'Embarrassment', 'Empathic Pain', 'Entrancement', 'Envy', 'Excitement', 'Fear', 'Guilt', 'Horror', 'Interest', 'Joy', 'Love', 'Nostalgia', 'Pain', 'Pride', 'Realization', 'Relief', 'Romance', 'Sadness', 'Satisfaction', 'Desire', 'Shame', 'Surprise (negative)', 'Surprise (positive)', 'Sympathy', 'Tiredness', 'Triumph']
 
-face_0 = face[face["Id"] == "face_0"]
-face_0_emotions = face_0[emotions].T
-face_1 = face[face["Id"] == "face_1"]
-face_1_emotions = face_1[emotions].T
-
-face_0_emotions['average'] = face_0_emotions.mean(numeric_only=True, axis=1)
-face_0_emotions['std'] = face_0_emotions.std(numeric_only=True, axis=1)
-face_0_emotions.sort_values(by=['average'], inplace=True, ascending=False)
-
-face_1_emotions['average'] = face_1_emotions.mean(numeric_only=True, axis=1)
-face_1_emotions['std'] = face_1_emotions.std(numeric_only=True, axis=1)
-face_1_emotions.sort_values(by=['average'], inplace=True, ascending=False)
-
-# print(face_0_emotions.head())
-
-print(prosody.columns)
-
-def process(df, id):
+def process(df, id, sort="average"):
     new_df = df[df["Id"] == id]
     new_df = new_df[emotions].T
     new_df['average'] = new_df.mean(numeric_only=True, axis=1)
     new_df['std'] = new_df.std(numeric_only=True, axis=1)
-    new_df.sort_values(by=['average'], inplace=True, ascending=False)
+    if sort:
+        new_df.sort_values(by=sort, inplace=True, ascending=False)
     return new_df
+
+face_viewer = process(face, "face_"+viewer_face)
+face_viewee = process(face, "face_"+viewee_face)
+pros_viewer = process(prosody, "spk_"+viewer_pros)
+pros_viewee = process(prosody, "spk_"+viewee_pros)
+
+
+## bar plot analysis (repeat for all)
+face_viewer_bar = face_viewer[["average"]][0:5]
+face_viewer_bar.to_csv("plot_data/face_0_barplot.csv")
+
+
+# timeline analysis
+top_viewer_emos = ["Interest", "Boredom", "Admiration", "Disappointment", "Surprise (positive)", "Surprise (negative)"]
+top_viewee_emos = ["Calmness", "Excitement", "Awkwardness", "Doubt","Anxiety","Triumph"]
+
+face_viewer_tl = face_viewer.drop(labels=['average', 'std'],axis=1)
+face_viewer_tl = face_viewer_tl.loc[top_viewer_emos]
+face_viewer_tl.columns = face["Time"][::2]
+
+print(face_viewer_tl)
